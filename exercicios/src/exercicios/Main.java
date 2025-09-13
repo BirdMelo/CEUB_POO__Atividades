52572845cccd90479ca86524d6d592fb0424ff0e
joao.naves <joao.naves@AN0100117065993.dinfor.uniceub>
2025-09-13 10:08:05 -0300

    atualização do dia 13/09

--- exercicios/src/exercicios/Main.java
+++ exercicios/src/exercicios/Main.java
@@ -3,56 +3,53 @@ package exercicios;
 import java.util.ArrayList;
 
 public class Main {
-	//ATIVIDADE 1 E 2
-	public static int unicos(ArrayList<Integer> list) {
-		ArrayList<Integer> unicos = new ArrayList<Integer>();
-		for(int i = 0; i < list.size(); i++) {
-			if(!unicos.contains(list.get(i))) {
-				unicos.add(list.get(i));
+	public static ArrayList<Integer> xorAdd(ArrayList<Integer> xorList, ArrayList<Integer> list){
+		for(Integer integer: list) {
+			if(!xorList.contains(integer)) {
+				xorList.add(integer);
 			}
 		}
+		return xorList;
+	}
+	public static int unicos(ArrayList<Integer> list) {
+		ArrayList<Integer> unicos = new ArrayList<Integer>();
+		xorAdd(unicos, list);
 		return unicos.size();
 	}
 	public static ArrayList<Integer> exclusivo(ArrayList<Integer> nums1, ArrayList<Integer> nums2) {
 		ArrayList<Integer> exclusivo = new ArrayList<Integer>();
-		for (Integer num : nums1) {
-			if(!nums2.contains(num)) {
-				exclusivo.add(num);
-			}
-		}
-		for (Integer num : nums2) {
-			if(!nums1.contains(num)) {
-				if(!exclusivo.contains(num)) {
-					exclusivo.add(num);
-				}
-			}
-		}
+		xorAdd(exclusivo, nums1);
+		xorAdd(exclusivo, nums2);
+		
 		return exclusivo;
 	}
 	public static void main(String[] args) {
 		
-		ArrayList<Integer> nums = new ArrayList<Integer>();
-		nums.add(2);
-		nums.add(3);
-		nums.add(5);
-		nums.add(2);
-		nums.add(22);
-		nums.add(90);
+		ArrayList<Integer> nums1 = new ArrayList<Integer>();
+		nums1.add(2);
+		nums1.add(3);
+		nums1.add(5);
+		nums1.add(2);
+		nums1.add(22);
+		nums1.add(90);
 		
 		ArrayList<Integer> nums2 = new ArrayList<Integer>();
-		nums.add(2);
-		nums.add(6);
-		nums.add(7);
-		nums.add(5);
-		nums.add(24);
-		nums.add(92);
+		nums2.add(2);
+		nums2.add(6);
+		nums2.add(7);
+		nums2.add(5);
+		nums2.add(24);
+		nums2.add(92);
 		
-		System.out.printf("tamanho de nums: %d\n", nums.size());
-		System.out.printf("Elemento 2 na lista?\n%b\n", nums.contains(2));
-		System.out.printf("Elemento 55 na lista?\n%b\n", nums.contains(55));
+		//ATIVIDADE 1
+		System.out.printf("tamanho de nums: %d\n", nums1.size());
+		System.out.printf("Elemento 2 na lista?\n%b\n", nums1.contains(2));
+		System.out.printf("Elemento 55 na lista?\n%b\n", nums1.contains(55));
 		
-		System.out.printf("Quantidade de valores unicos em nums: %d\n", unicos(nums));
-		System.out.println(exclusivo(nums, nums2));
+		//ATIVIDADE 2
+		System.out.printf("Quantidade de valores unicos em nums: %d\n", unicos(nums1));
+		//ATIVIDADE 3
+		System.out.println(exclusivo(nums1, nums2));
 		
 	}
 	
